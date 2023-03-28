ed304f190f7a1d9fcf6e4e313a7f8b10bc6d1830
张庙松 <1875912440@qq.com>
2023-03-28 16:29:01 +0800

    输入输出初步分离

--- code_and_data/new.py
+++ code_and_data/new.py
@@ -13,16 +13,14 @@ import csv
 from spmf.mining import *
 from CPT import *
 import operator
-# 保存挖掘出的频繁序列
-output_freseqs_path = '.\\output\\freseq.csv'
-# 保存挖掘出的规则项集
-output_assorules_path = '.\\output\\assorules.csv'
 
-file_path = '.\\t_alarmloghist_1_1.csv'
+
+file_path = '.\\t_alarmlogcur.csv'
 output_path = '.\\output\\predict_rusult.csv'
 if __name__ == '__main__':
     # 数据输入
     data = dp.read_data(file_path)
+    #data = TrainRequest
     print('数据内部清洗前长度为', len(data))
     # 清洗数据，按照时间，端口号等进行聚类
     data = dp.Datacluster(data,eps = 0.005)
@@ -31,16 +29,11 @@ if __name__ == '__main__':
     #  所有数据的clocationinfo信息列表locs，反索引列表locToIndexOflocs，loc对应的网元id、端口信息、定位信息列表locstoneid
     locs, locToIndexOflocs, locstoneid = dp.getContextofLocs(data)
 
-    # 保存变量到文件
+    # 保存反索引到文件
     with open("locs.pkl", "wb") as f:
         pickle.dump(locs, f)
-    with open("locToIndexOflocs.pkl", "wb") as f:
-        pickle.dump(locToIndexOflocs, f)
-    with open("locstoneid.pkl", "wb") as f:
-        pickle.dump(locstoneid, f)
-
     ## 生成所有时间序列
-    Seqs = dp.GenSeqs(data, 100)
+    Seqs = dp.GenSeqs(data, 250)
 
 
     # 对序列进行字符串化,去除序列中每个项目集的重复项
@@ -53,19 +46,12 @@ if __name__ == '__main__':
     maxseqlen = max([len(Seq) for Seq in Seqs])
 
     ## 频繁序列挖掘 SPAM
-    freqSeqs = Mining_FreSeqPatterns(Seqs,minsp=12/len(Seqs),minpatternlen=2,maxpatternlen=10,maxgap=100)
+    freqSeqs = Mining_FreSeqPatterns(Seqs,minsp=25/len(Seqs),minpatternlen=2,maxpatternlen=10,maxgap=100)
     freqSeqs = sorted(freqSeqs,key = lambda x: x[-1])
-    # 保存挖掘出的频繁序列
-    column_Seq = pd.Series([x[0] for x in freqSeqs],name='Seq')
-    column_Seq_detailed = pd.Series([[[locstoneid[locs[int(item)]] for item in items] for items in x[0]] for x in freqSeqs],name='Seq_detailed') #具体点的
-    column_sup = pd.Series([x[1] for x in freqSeqs],name='sup')
-    con = pd.concat([column_Seq,column_Seq_detailed,column_sup],axis=1)
-    con.to_csv(output_freseqs_path,index = True,sep=',',mode='w+',encoding='utf_8_sig')
-
     ## 序列预测 CPT
     Seqs = dp.Seqs_dim_reduction(Seqs)
 
-    # 筛掉只有长度为1的序列（长度都为1了还预测个啥QAQ）
+    # 筛掉只有长度为1的序列
     Seqs = [seq for seq in Seqs if len(seq) > 1]
 
     model = CPT()
@@ -79,3 +65,5 @@ if __name__ == '__main__':
     # 保存模型到文件
     with open('model.pkl', 'wb') as f:
         pickle.dump(model, f)
+    with open('train.pkl', 'wb') as f:
+        pickle.dump(train, f)
